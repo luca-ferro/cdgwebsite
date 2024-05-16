b75ae28e667da63492a624a40586a36df92e4223
Luca Ferro Oliveira <luca.oliveira@polygonsolucoes.com.br>
2024-05-16 15:31:35 -0300

    front and back

--- backend/src/main/java/org/example/rifacdg/dto/BuyerDTO.java
+++
@@ -1,4 +0,0 @@
-package org.example.rifacdg.dto;
-
-public record BuyerDTO(String name, String phone, String mail, String seller, int quantity){
-}
--- backend/src/main/java/org/example/rifacdg/repository/BuyerRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.rifacdg.repository;
-
-import org.example.rifacdg.model.Buyer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BuyerRepository extends JpaRepository<Buyer, Integer> {
-}
